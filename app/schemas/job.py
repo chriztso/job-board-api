0a9ff2ae8df2801601d8e6c9bc4f7dd9c4f87bd9
Christopher Tso <christophertso@Christophers-iMac.local>
2025-04-20 17:50:50 -0500

    added api schema

---
+++ app/schemas/job.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+class JobBase(BaseModel):
+    title: str
+    company: str
+    location: str
+
+class JobCreate(JobBase):
+    pass
+
+class Job(JobBase):
+    id: int
+
+    class Config:
+        orm_mode = True
