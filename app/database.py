88bc7b22b4a50c8a925d02d077d9829367666a05
Christopher Tso <christophertso@Christophers-iMac.local>
2025-04-20 17:36:39 -0500

    added database

---
+++ app/database.py
@@ -0,0 +1,25 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
+
+if not SQLALCHEMY_DATABASE_URL:
+    raise ValueError("SQLALCHEMY_DATABASE_URL is not set")
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def test_db_connection():
+    try:
+        with SessionLocal() as db:
+            db.execute("SELECT 1")
+            print("Database connection successful")
+    except Exception as e:
+            print(f"Database connection failed: {e}")
+        
