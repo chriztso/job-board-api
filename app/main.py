ba2e113a0c4d1a34898c8d10a61bdddb3e919814
Christopher Tso <christophertso@Christophers-iMac.local>
2025-04-20 17:38:44 -0500

    test db connection on startup

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from app.api.routes import users, jobs
-
+from app.database import test_db_connection
 app = FastAPI()
+
+@app.on_event("startup")
+def startup_db_client():
+    test_db_connection()
 
 app.include_router(jobs.router, prefix="/api/jobs", tags=["jobs"])
