413dab791042a58389b2fe0bc6283b98b823976e
Christopher Tso <christophertso@Christophers-iMac.local>
2025-04-21 00:23:08 -0500

    edited post route to save to database

--- app/api/routes/jobs.py
+++ app/api/routes/jobs.py
@@ -1,6 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 from typing import List
 from app.schemas.job import Job, JobCreate
+from app.database import get_db
+from app.models.job import JobModel
 router = APIRouter()
 
 @router.get("/", response_model=List[Job])
@@ -11,5 +14,13 @@ def get_jobs():
             ]
 
 @router.post("/", response_model=Job)
-def create_job(job: dict):
-    return {"message": "Job created successfully", "job": job}
+def create_job(job: JobCreate, db: Session = Depends(get_db)):
+    db_job = JobModel(
+        title=job.title,
+        company=job.company,
+        location=job.location
+    )
+    db.add(db_job)
+    db.commit()
+    db.refresh(db_job)
+    return db_job
